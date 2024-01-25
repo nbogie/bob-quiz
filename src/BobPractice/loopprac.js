1c79857fbc3b6f47b287d2231ad4e2cbe079f9bf
nbogie <neillbogie@googlemail.com>
2024-01-25 14:55:03 +0000

    with objects

--- src/BobPractice/loopprac.js
+++ src/BobPractice/loopprac.js
@@ -1,23 +1,37 @@
 const { prompt } = require("enquirer");
 
-let counter;
-
 async function mainLoop() {
-  for (counter = 1; counter <= 7; counter++) {
-    await higherOrLower();
+  const MAX_ROUND_NUMBER = 7;
+  let counter = 1;
+  let currentNumber = randomInt();
+  while (counter <= MAX_ROUND_NUMBER) {
+    const result = await playOneIterationOfHigherLower(currentNumber);
+    if (result.outcome === "success") {
+      currentNumber = result.number;
+    } else {
+      //must have failed
+      break;
+    }
+    counter++;
+  }
+  if (counter > MAX_ROUND_NUMBER) {
+    console.log("WOOO HOO CONGRATS");
   }
 }
 
 mainLoop();
 
-async function higherOrLower() {
-  const currentNumber = randomInt();
+/**
+ *@param {number} currentNumner
+ * @returns {Promise<{outcome: "success", number:number } | {outcome: "fail"} >}
+ */
+async function playOneIterationOfHigherLower(currentNumber) {
   const nextNumber = randomInt(); // intend to compare then set current to next
 
   const userResultPrompt = await prompt({
     type: "input",
     name: "userResponse",
-    message: `The number is ${currentNumber}. Will the next number (1-13) be higher or lower?`,
+    message: `The number is ${currentNumber}. Will the next number (1-13) be higher or lower? (CHEAT: ${nextNumber})`,
   });
 
   const guess = userResultPrompt.userResponse.toLowerCase();
@@ -30,14 +44,15 @@ async function higherOrLower() {
         counter + 1
       }.`
     );
+    return { outcome: "success", number: nextNumber };
   } else if (nextNumber === currentNumber) {
     console.log(`Unlucky! The next number is also ${nextNumber}. You lose.`);
-    counter = 11;
+    return { outcome: "fail" };
   } else {
     console.log(
       `You said ${guess}. This was wrong. The next number is ${nextNumber}. Go home.`
     );
-    counter = 11;
+    return { outcome: "fail" };
   }
 
   return userResultPrompt;
